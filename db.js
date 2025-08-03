7a6e0f32bab92c5d80d8189773b594050388290b
saiKelkar <kelkars1999@gmail.com>
2025-08-03 18:12:06 +0530

    Add dotenv support for environment variables
    Replaced hardcoded database and server configuration with environment variables using dotenv. Updated .gitignore to exclude .env files and added dotenv as a dependency.

--- db.js
+++ db.js
@@ -2,11 +2,11 @@ import pkg from "pg"
 const { Pool } = pkg
 
 const pool = new Pool({
-    user: "postgres",
-    host: "localhost",
-    database: "myarticledb",
-    password: "password",
-    port: 5432,
+    user: process.env.DB_USER,
+    host: process.env.DB_HOST,
+    database: process.env.DB_NAME,
+    password: process.env.DB_PASSWORD,
+    port: process.env.DB_PORT,
 })
 
 pool.connect()
--- server.js
+++ server.js
@@ -1,8 +1,10 @@
 import express from "express"
+import dotenv from 'dotenv'
 import articleRoutes from './routes/articleRoutes.js'
 
 const app = express()
-const port = 3000
+const port = process.env.PORT || 3000
+dotenv.config()
 
 app.use(express.json())
 app.use('/article', articleRoutes)
